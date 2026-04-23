c269b33b5800915a278434541fde95c700127590
Tanya Ampilogova <ampi.tatyana@gmail.com>
2026-04-23 15:20:40 -0700

    initial project

---
+++ BeautyCheckList/BeautyCheckList/BeautyCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyCheckListApp.swift
+//  BeautyCheckList
+//
+//  Created by Tatiana Ampilogova on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BeautyCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeautyCheckList/BeautyCheckList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BeautyCheckList
+//
+//  Created by Tatiana Ampilogova on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
